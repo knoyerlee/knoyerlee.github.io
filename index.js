ce4cb111a60defa13fc548f9fa0f87a5eee81f62
Lee Knoyer <lknoyer@Lees-MacBook-Pro.local>
2022-09-25 21:32:42 -0400

    website v2

---
+++ index.js
@@ -0,0 +1,6 @@
+let pythonLink = document.getElementById("pythonLink");
+let pandasLink = document.getElementById("pandasLink");
+let iacLink = document.getElementById("iacLink");
+let terraformLink = document.getElementById("terraformLink");
+let jenkinsLink = document.getElementById("jenkinsLink");
+let angularLink = document.getElementById("angularLink");
